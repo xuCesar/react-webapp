a7d4366fdee09769ff93a55b8fffb20623a48e1d
cesar <49577231@qq.com>
2020-03-12 14:55:31 +0800

    修改导航

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Row, Col, Menu, Icon } from 'antd'
 import { withRouter } from 'react-router-dom'
 import './index.scss'
 
@@ -18,11 +19,23 @@ class Nav extends Component {
   render() {
     return (
       <div className='nav-wrap'>
-        <ul className='nav-list'>
-          <li onClick={this.handleClickNav}>首页</li>
-          <li>笔记</li>
-          <li>读书</li>
-        </ul>
+        
+        <Row className='nav-bar' align='middle' justify='center'>
+          {/* <Col  xs={24} sm={24} md={10} lg={10} xl={10}>
+              <span className="header-logo">XXX</span>
+          </Col> */}
+          <Col span={6}>
+          <span className="header-logo">XXX</span>
+          </Col>
+          <Col className='nav-list' span={18}>
+            <Menu mode='horizontal'>
+              <Menu.Item onClick={this.handleClickNav}> 首页 </Menu.Item>
+              <Menu.Item> 笔记 </Menu.Item>
+              <Menu.Item> 读书 </Menu.Item>
+            </Menu>
+          </Col>
+          
+        </Row>
       </div>
     )
   }
