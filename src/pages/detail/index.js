89048ebad1c0b62b170da576bfa60d8dd1b3f990
cesar <49577231@qq.com>
2020-03-13 01:00:27 +0800

    编写文字详情页

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,10 +1,74 @@
 import React, { Component } from 'react'
+import {Row, Col ,Affix, Icon ,Breadcrumb  } from 'antd'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css'
 
+import './index.scss'
+
+let markdown = 
+  '# p01:课程介绍和环境搭建 \n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+   '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World \n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
 export default class Detail extends Component {
   render() {
     return (
       <div className='content'>
-        Detail
+        <Row justify='space-between'>
+          <Col span={15}>
+            <div className='content-left'>
+              <ReactMarkdown 
+                source={markdown}
+                escapeHtml={false}
+              />
+            </div>
+          </Col>
+
+          <Col span={6}>
+            <Affix offsetTop={5}>
+              <div className="content-right">
+                <div className="nav-title">文章目录</div>
+                  <MarkNav
+                    className="article-menu"
+                    source={markdown}
+                    ordered={false}
+                  />
+              </div>
+            </Affix>
+          </Col>
+        
+        </Row>
       </div>
     )
   }
