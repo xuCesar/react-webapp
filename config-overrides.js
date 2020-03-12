cdb7c6f4e463e4ca2bf6fe07576ebb025a8e5671
cesar <49577231@qq.com>
2020-03-12 13:38:11 +0800

    change config override

--- config-overrides.js
+++ config-overrides.js
@@ -1,41 +1,21 @@
-// const { override, fixBabelImports } = require('customize-cra')
-
-// module.exports = function override(config, env) {
-//   console.log('override')
-//   return config
-// }
-
-// module.exports = override(
-//   fixBabelImports('import', {
-//     libraryName: 'antd',
-//     libraryDirectory: 'es',
-//     style: 'css'
-//   })
-// )
-
-// const {injectBabelPlugin} = require('react-app-rewired');
-// const rewireLess = require('react-app-rewire-less');
-// const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
 const path = require('path');
 
-module.exports = function override(config, env) {
-    config.resolve.alias['@src'] = resolve('src')
-    config.resolve.alias['@cp'] = resolve('src/components')
-    config.resolve.alias['@images'] = resolve('src/components')
-    //do stuff with the webpack config...
-    // config = rewireLess(config, env);
-    // config = injectBabelPlugin(['import', {libraryName: 'antd', style: true}], config);
-    //config = injectBabelPlugin(['transform-decorators-legacy'], config);
-    //config = injectBabelPlugin(['transform-decorators'], config);
-    // const modifyVars = {
-    //     '@primary-color': '#00B7D8',
-    //     '@error-color': '#faad14',
-    // };
-    // config = rewireLess.withLoaderOptions({modifyVars: modifyVars})(config, env);
-    // config = rewireReactHotLoader(config, env);
-    return config;
-};
-
-function resolve(dir) {
-    return path.join(__dirname, dir);
+const resolve = dir => {
+  return path.join(__dirname, dir);
 }
+
+module.exports = override(
+  // antd按需加载，不需要每个页面都引入"antd/dist/antd.css"了
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  // 配置路径别名
+  addWebpackAlias({
+    '@': resolve('src'),
+    '@cp': resolve('src/components'),
+    '@img': resolve('src/images')
+  })
+)
